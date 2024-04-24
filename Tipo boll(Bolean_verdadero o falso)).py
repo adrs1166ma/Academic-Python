98ddacfc2528f1e57bff7fd8a7040899189efedc
Jeampierbq <Jeampierbarrios04@gmail.com>
2024-04-23 19:30:59 -0500

    tipo boll

---
+++ Tipo boll(Bolean_verdadero o falso)).py
@@ -0,0 +1,10 @@
+#Tipos boll (Bolean)
+mivariable = False
+print( mivariable )
+
+mivariable = 3 > 4
+
+if mivariable :
+    print("El resultado es Verdadero")
+else :
+    print("El resultado fue falso")
