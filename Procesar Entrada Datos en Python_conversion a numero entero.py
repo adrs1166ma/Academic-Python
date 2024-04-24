ac02f935ffc7954d5b8f8becac47bf3200d77246
Jeampierbq <Jeampierbarrios04@gmail.com>
2024-04-24 11:26:18 -0500

    sesion1_Datosdeentrada

---
+++ Procesar Entrada Datos en Python_conversion a numero entero.py
@@ -0,0 +1,8 @@
+#Funcion input para procesar la entrada del usuario
+#En este caso el input se convirtio a int por lo general es de tipo str
+numero1 = int(input("Escribe el primer numero :"))
+numero2 = int(input("Escribe el segundo numero :"))
+resultado = numero1 + numero2
+print("La suma es :", resultado)
+
+print("Fin del programa")
