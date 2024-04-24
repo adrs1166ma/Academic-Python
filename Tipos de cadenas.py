05134115d64ffe348457f4da9c465ae698e29d8b
Jeampierbq <Jeampierbarrios04@gmail.com>
2024-04-23 19:13:07 -0500

    sesion1_tipodecadenas

---
+++ Tipos de cadenas.py
@@ -0,0 +1,14 @@
+#Tipos de cadenas
+micantantefavorito = "Big soto"
+cancion = "Pegao"
+#print("Mi cantante favorito es :" + micantantefavorito)
+
+print("Mi cantante favorito es:",micantantefavorito,cancion)
+
+#Para converir de tipo str a tipo int
+
+numero1 = "1"
+numero2 = "2"
+print("La suma es :", int(numero1) + int(numero2))
+
+
